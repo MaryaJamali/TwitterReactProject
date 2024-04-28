2e50b46cc8ae35ae19016d45eee1267692d7f359
MaryamJamali  ❤️ <150019136+MaryaJamali@users.noreply.github.com>
2024-04-28 14:37:05 +0330

    coding

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from "./components/App";
+import ThemeProvider from "@material-ui/styles/ThemeProvider";
+import theme from './components/theme';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-        <App></App>
-    </React.StrictMode>
+    <ThemeProvider theme={theme}>
+        <React.StrictMode>
+            <App></App>
+        </React.StrictMode>
+    </ThemeProvider>
 );
