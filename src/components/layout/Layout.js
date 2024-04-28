dfe32537914ed6040e3a266336a9d419f5563d1d
MaryamJamali  ❤️ <150019136+MaryaJamali@users.noreply.github.com>
2024-04-28 16:05:50 +0330

    Twitter project home page design

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import useStyle from './styles'
+import Typography from "@material-ui/core/Typography";
 
 
 const Layout = () => {
     const classes = useStyle();
     return (
         <div className={classes.root}>
-
+            <Typography>توییتر</Typography>
         </div>
     );
 };
---
+++ src/components/theme/index.js
@@ -0,0 +1,35 @@
+import {createTheme} from "@material-ui/core";
+import tinyColor from 'tinycolor2'
+
+const colorPrimary = "#5ea9dd";
+
+const Theme = createTheme({
+    palette: {
+        primary: {
+            main: colorPrimary,
+            light: tinyColor(colorPrimary).lighten().toHexString()
+        }
+    },
+    overrides: {
+        MuiTypography: {
+            root: {
+                // !important means priority is given to this line of code
+                // for example, if we gave a fontFamily somewhere and defined another fontFamily here,
+                // priority is applied to this fontFamily and this fontFamily.
+                fontFamily: "shabnam !important",
+            }
+        },
+        MuiListItem: {
+            button: {
+                fontFamily: "shabnam !important",
+            }
+        },
+        MuiButton: {
+            label: {
+                fontFamily: "shabnam !important",
+            }
+        }
+    }
+})
+
+export default Theme;
