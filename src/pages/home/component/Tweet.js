099e46d3cd0fa37889c42cc2b7e0c24fe2a8453c
MaryamJamali  ❤️ <150019136+MaryaJamali@users.noreply.github.com>
2024-04-29 18:56:43 +0330

    Coding the Tweet part

---
+++ src/pages/home/component/Tweet.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import useStyle from '../styles'
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+
+
+const Tweet = ({data}) => {
+    const classes = useStyle();
+    return (
+        <div className={classes.tweetItem}>
+            <Grid container>
+                <img src={data.sender.img} className={classes.userImg}/>
+                <Grid item container direction={"column"} style={{flex: 1, marginRight: '1rem'}}>
+                    <Grid item container>
+                        <Typography className={classes.tweetItemName}>{data.sender.name}</Typography>
+                        <Typography className={classes.tweetItemId}>{data.sender.id}</Typography>
+                    </Grid>
+                    <Typography className={classes.tweetText}>{data.text}</Typography>
+                </Grid>
+            </Grid>
+            <Grid container direction={"row-reverse"} style={{marginTop: 16}} alignItems={'center'}>
+                <IconButton className={classes.newTweetImgBtn}>
+                    <img src={"/images/retweet.png"} className={classes.newTweetImg}/>
+                </IconButton>
+                <IconButton className={classes.newTweetImgBtn}>
+                    <FavoriteIcon style={{color: 'red'}}/>
+                </IconButton>
+                <Typography className={classes.likeCount}>{data.like}</Typography>
+            </Grid>
+        </div>
+    );
+};
+
+export default Tweet;
