665ea9bf8766d7ae614bb9ed87e4864e18954be2
MaryamJamali  ❤️ <150019136+MaryaJamali@users.noreply.github.com>
2024-04-30 17:09:19 +0330

    Coding the header part

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import useStyle from '../styles'
 import Typography from "@material-ui/core/Typography";
-import HomeIcon from "@material-ui/icons/Home";
+import useStyle from './styles'
 
-
-const Header = () => {
+const Header = ({title, icon}) => {
     const classes = useStyle();
     return (
         <div className={classes.header}>
-            <HomeIcon/>
+            {icon}
             <Typography className={classes.headerTitle}>
-                خانه
+                {title}
             </Typography>
         </div>
     );
