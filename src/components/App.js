969d7b4b8c2a743666c7a5dd59b0010538016476
MaryamJamali  ❤️ <150019136+MaryaJamali@users.noreply.github.com>
2024-05-01 20:09:37 +0330

    Define routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import Layout from "./layout/Layout";
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Home from "../pages/home/Home";
+import TweetsByHashTag from "../pages/tweetsByHashtag/TweetsByHashTag";
+import TweetsByLanguage from "../pages/tweetsByLanguage/TweetsByLanguage";
+import Page404 from "../pages/page404/Page404";
+import {Switch} from "@material-ui/core";
 
 const App = () => {
     return (
-        <Layout>
-
-        </Layout>
+        <BrowserRouter>
+            <Routes>
+                <Route path={"/"} render={() => {
+                    return <Layout>
+                        <Switch>
+                            <Route exact path={"/"} component={Home}/>
+                            <Route exact path={"/hashtags/:hashtag"} component={TweetsByHashTag}/>
+                            <Route exact path={"/languages/:name"} component={TweetsByLanguage}/>
+                            <Route component={Page404}/>
+                        </Switch>
+                    </Layout>
+                }}/>
+            </Routes>
+        </BrowserRouter>
     );
 };
 
--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -2,20 +2,16 @@ import useStyle from './styles'
 import Divider from "@material-ui/core/Divider";
 import RightSidebar from "../rightSidebar/RightSidebar";
 import LeftSidebar from "../leftSidebar/LeftSidebar";
-import Home from "../../pages/home/Home";
-import TweetsByHashTag from "../../pages/tweetsByHashtag/TweetsByHashTag";
-import TweetsByLanguage from "../../pages/TweetsByLanguage/TweetsByLanguage";
-import {BrowserRouter, Routes} from 'react-router-dom';
-import Page404 from "../../pages/page404/Page404";
 
-const Layout = () => {
+
+const Layout = (props) => {
     const classes = useStyle();
     return (
         <div className={classes.root}>
             <RightSidebar/>
             <Divider orientation={"vertical"} className={classes.divider}/>
             <div className={classes.content}>
-                <Home/>
+                {props.children}
             </div>
             <Divider orientation={"vertical"} className={classes.divider}/>
             <LeftSidebar/>
