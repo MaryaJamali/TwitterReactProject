39bc7ae3d59f1e35c353fdaac16e191eb0f01848
MaryamJamali  ❤️ <150019136+MaryaJamali@users.noreply.github.com>
2024-05-01 20:08:30 +0330

    Reforms

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <ThemeProvider theme={theme}>
         <React.StrictMode>
-            <App></App>
+            <App/>
         </React.StrictMode>
     </ThemeProvider>
 );
